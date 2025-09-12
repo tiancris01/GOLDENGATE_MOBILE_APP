0f9e84baed2e2ed727fae3cbd9b01fff6249b875
Daniel osorio <tiancris01@gmail.com>
2025-09-12 14:14:15 -0500

    [ft][GG-013] Add user model, serialization, and Firestore integration

---
+++ packages/auth/lib/src/model/custom_error.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'custom_error.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_CustomError _$CustomErrorFromJson(Map<String, dynamic> json) => _CustomError(
+  code: json['code'] as String? ?? '',
+  message: json['message'] as String? ?? '',
+);
+
+Map<String, dynamic> _$CustomErrorToJson(_CustomError instance) =>
+    <String, dynamic>{'code': instance.code, 'message': instance.message};
