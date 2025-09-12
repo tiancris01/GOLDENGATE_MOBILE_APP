32fc456de0d305b8b1197a62c3cc88e4515fe60a
Tiancris <103019305+tiancris01@users.noreply.github.com>
2025-09-12 14:19:27 -0500

    [ft][GG-013] Add user model, serialization, and Firestore integration (#13)

---
+++ packages/auth/lib/src/model/custom_error.dart
@@ -0,0 +1,15 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'custom_error.freezed.dart';
+part 'custom_error.g.dart';
+
+@freezed
+abstract class CustomError with _$CustomError {
+  const factory CustomError({
+    @Default('') String code,
+    @Default('') String message,
+  }) = _CustomError;
+
+  factory CustomError.fromJson(Map<String, dynamic> json) =>
+      _$CustomErrorFromJson(json);
+}
